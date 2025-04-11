e76dfe65e708f6253d903e92bed60ba2c5ed77aa
JustNap <justinvalen04@gmail.com>
2025-04-11 22:17:48 +0700

    menambahkan fitur setting

--- apk_pembukuan/lib/main.dart
+++ apk_pembukuan/lib/main.dart
@@ -1,19 +1,26 @@
 import 'package:flutter/material.dart';
 import 'Piutang/piutang.dart';
-
+// import 'Setting/setting.dart';
 void main() {
   runApp(MyApp());
 }
 
+final ValueNotifier<ThemeMode> themeNotifier = ValueNotifier(ThemeMode.light);
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Piutang App',
-      theme: ThemeData(
-        primarySwatch: Colors.purple,
-      ),
-      home: PiutangPage(),
+    return ValueListenableBuilder<ThemeMode>(
+      valueListenable: themeNotifier,
+      builder: (context, currentTheme, _) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData.light(),
+          darkTheme: ThemeData.dark(),
+          themeMode: currentTheme,
+          home: PiutangPage(), //SettingPage(), 
+        );
+      },
     );
   }
 }
